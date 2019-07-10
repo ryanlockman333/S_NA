ff6bfb9c4b5b364a43bb2efae73a151c1689abbe
Ryan Lockman <ryanlockman333@gmail.com>
2019-07-10 14:01:09 -0600

    Update sniffer.h

--- sniffer.h
+++ sniffer.h
@@ -1,7 +1,7 @@
 /*
  * sniffer.h
  * Description: Sniffer lass prototypes for netwrok sniffing.
- * © 2015 Ryan Lockman All Rights Reserved
+ * © 2015 Ryan D. Lockman All Rights Reserved
  * )))~3L1735~(((
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
